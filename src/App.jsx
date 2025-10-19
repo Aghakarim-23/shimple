9293032f913661efa956b6d509eef3b9044c320b
Aghakarim-23 <hemidzade1010@gmail.com>
2025-10-19 23:36:19 +0400

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const [message, setMessage] = useState("")
+  useEffect(() => {
+    const getLink = async () => {
+      try {
+       const response = await axios.get("https://dashboard.render.com/web/srv-d3cejh56ubrc73en9250/logs")
+        setMessage(response.data)
+      } catch (error) {
+        console.error(error);
+      } 
+    }
+
+    getLink()
+  },[])
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      {message}
+    </div>
+  )
+}
+
+export default App
