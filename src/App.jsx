e07ab615919bc39acd1b4120e183aca73d095e86
Aghakarim-23 <hemidzade1010@gmail.com>
2025-10-19 23:38:08 +0400

    axios imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-
+import axios from 'axios'
 const App = () => {
   const [message, setMessage] = useState("")
   useEffect(() => {
